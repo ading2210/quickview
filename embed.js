025a1686518bdd76cda73619aba722f7e711c342
ading2210 <ading2019@gmail.com>
2023-04-29 23:52:45 -0700

    add warnings for non chromeos devices and zoom

--- embed.js
+++ embed.js
@@ -1,7 +1,28 @@
 let cros = /(CrOS)/.test(navigator.userAgent);
+let warnings = {};
 
 function from_id(id) {
-  return document.getElementById(id)
+  return document.getElementById(id);
+}
+
+function update_warnings() {
+  let warning_div = from_id("warning_div");
+  warning_div.innerHTML = "";
+  for (let warning of Object.values(warnings)) {
+    warning_div.innerHTML += `<p><b>Warning:</b> ${warning}</p>`;
+  }
+  warning_div.style.display = Object.keys(warnings).length ? "block" : "none";
+}
+
+function test_zoom() {
+  let zoom_test = from_id("zoom_test");
+  if (zoom_test && zoom_test.clientWidth * window.devicePixelRatio !== 100) {
+    warnings.zoom = "You need to set your browser zoom as well as display size to 100%.";
+  }
+  else if (warnings.zoom) {
+    delete warnings.zoom;
+  }
+  update_warnings();
 }
 
 window.onload = () => {
@@ -13,7 +34,10 @@ window.onload = () => {
     }, 5000);
   }
   else {
-    alert("This only works on Chrome OS!");
-    container.remove;
+    warnings.cros = "This only works on Chrome OS!";
+    update_warnings();
   }
+
+  test_zoom();
 }
+window.onresize = test_zoom;
